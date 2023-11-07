92b461d6925847f6489de23498c1ee959cdb2bdb
Shaneen <jawalinikita06@gmail.com>
2023-11-07 20:37:56 +0530

    code added

--- JobService/src/main/java/com/job/exception/ResourceNotFoundException.java
+++ JobService/src/main/java/com/job/exception/ResourceNotFoundException.java
@@ -2,7 +2,9 @@ package com.job.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
 
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public ResourceNotFoundException() {
 		super("Reasource not found");
 	}
